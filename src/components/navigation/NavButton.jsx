3c4f54217cf3173da945a5153c1591c341650c1c
exy <teslawiktor@gmail.com>
2025-01-15 12:14:56 +0100

    56:06

---
+++ src/components/navigation/NavButton.jsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import React from 'react'
+
+const NavButton = ({x,y, label, link, icon, newTab}) => {
+  return (
+    <div className='absolute cursor-pointer z-50'
+    style={{transform:`translate(${x}, ${y})`}}
+    >
+
+        <Link href={link} target={newTab ? '_blank' : '_self'} className='text-foreground rounded-full flex items-center justify-center' aria-label={label} name={label}>
+            <span>
+                {icon}
+            </span>
+        </Link>
+
+    </div>
+  )
+}
+
+export default NavButton
